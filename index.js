b05e3b4e7f0e51b4706eed7f834546d3651a671e
lrmoore2023 <lrmoore2023@gmail.com>
2023-08-06 22:59:20 -0400

    changes

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 $(document).ready(function() {
-    $('a.nav-link').click(function(e) {
+  $('a.nav-link').click(function(e) {
       e.preventDefault();
       var targetSection = $(this).attr('href');
       $('html, body').animate({
-        scrollTop: $(targetSection).offset().top
+          scrollTop: $(targetSection).offset().top - 110 // Adjust the offset value as needed
       }, 1000);
-    });
   });
+});
